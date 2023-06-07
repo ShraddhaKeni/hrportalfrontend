e609a026b3b9eb5394a48b2a49b6a730cc01dc41
shraddha <shraddha.keni@axzora.com>
2023-06-07 11:59:45 +0530

    changes

--- src/pages/Helper/Auth.js
+++ src/pages/Helper/Auth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export async function loginCheck (payload){
     try {
-        const {data} = await axios.get(`/users/${payload.id}`)
+        const {data} = await axios.get(`http://localhost:3001/users/${payload.id}`)
         return data.data
     } catch (error) {
         console.log(error)

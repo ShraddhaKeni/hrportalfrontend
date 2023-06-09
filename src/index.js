9459506385b7b8477ccd42b0a5f1bdd9c28bf145
shraddha <shraddha.keni@axzora.com>
2023-06-09 17:12:13 +0530

    updated index page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import { AuthProvider } from './pages/Helper/AuthProvider';
-
+import axios from 'axios';
+axios.defaults.baseURL = 'http://hr-portal.axzora.in:3000'
 ReactDOM.render(
   <BrowserRouter>
     <AuthProvider>
